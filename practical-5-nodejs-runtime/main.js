71f0253e3aa8b50c171ba3148e7a9325d369a958
Mohammad Afzal Malik <mdafzalmalik13@gmail.com>
2026-02-24 13:57:28 +0530

    Completed Practical 5 - Node.js Runtime using maths module

---
+++ practical-5-nodejs-runtime/main.js
@@ -0,0 +1,6 @@
+// main.js
+
+const maths = require('./maths');
+
+console.log("Addition:", maths.add(5, 3));
+console.log("Multiplication:", maths.multiply(5, 3));
---
+++ practical-5-nodejs-runtime/maths.js
@@ -0,0 +1,14 @@
+// utility.js
+
+function add(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+module.exports = {
+    add,
+    multiply
+};
