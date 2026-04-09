4b7999b1b90f9ad5d8850f1aded48fc370864c99
mdafzalmalik <mdafzalmalik13@gmail.com>
2026-04-09 09:24:11 +0530

    Completed Practical 15 - Express middleware and gitignore

---
+++ practical-15-express-middleware/server.js
@@ -0,0 +1,35 @@
+// server.js
+
+const express = require('express');
+const app = express();
+
+// Middleware 1: Logger
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+
+// Middleware 2: Simple authentication
+const auth = (req, res, next) => {
+    const isLoggedIn = true;
+
+    if (isLoggedIn) {
+        next();
+    } else {
+        res.send("Unauthorized Access");
+    }
+};
+
+app.use(logger);
+
+app.get('/', (req, res) => {
+    res.send("Home Page");
+});
+
+app.get('/dashboard', auth, (req, res) => {
+    res.send("Welcome to Dashboard");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
